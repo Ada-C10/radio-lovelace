50ecac2bb4c4649b5b0ce746bc76e533f85d283e
Aurea <fu.sakura@gmail.com>
2018-11-30 14:56:48 -0800

    convert Tracks to class component

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 import "./styles/Track.css";
 
 // Here we use destructuring to extract the props into separate variables
 // See https://wesbos.com/destructuring-objects/
-const Track = ({title, artist, playtime, albumart, favorite}) => {
-  return (
-    <li className="track">
-      <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
-      <h3 className="track--title">{title}</h3>
-      <input
-        type="checkbox"
-        className="track--favorite"
-        checked={!favorite}
-      />
-      <p className="track--artist">{artist}</p>
-      <p className="track--playtime">{playtime}</p>
-      <button
-        className="track--control track--to-top"
-        >
-        <span role="img" aria-label="send to top">🔝</span>
-      </button>
-      <button
-        className="track--control track--switch"
-        >
-        <span role="img" aria-label="switch lists">↔</span>
-      </button>
-    </li>
-  );
-};
+
+class Track extends Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    const {title, artist, playtime, albumart, favorite} = this.props;
+    return (
+
+      <li className="track">
+        <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
+        <h3 className="track--title">{title}</h3>
+        <input
+          type="checkbox"
+          className="track--favorite"
+          checked={!favorite}
+        />
+        <p className="track--artist">{artist}</p>
+        <p className="track--playtime">{playtime}</p>
+        <button
+          className="track--control track--to-top"
+          >
+          <span role="img" aria-label="send to top">🔝</span>
+        </button>
+        <button
+          className="track--control track--switch"
+          >
+          <span role="img" aria-label="switch lists">↔</span>
+        </button>
+      </li>
+    );
+  };
+
+}
+
 
 Track.propTypes = {
   title: PropTypes.string,

7c9b7de6f3179809cfb7328f1e4c5b6595870e1f
Katricia Smith <smithkatricia@gmail.com>
2018-12-05 15:25:31 -0800

    Move const playlists from RadioSet render to RadioSet constructor state

--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -6,18 +6,20 @@ import Playlist from './Playlist';
 class RadioSet extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      playlists: {
+        morningTracks: this.props.tracks.slice(0, props.tracks.length / 2),
+        eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
+      }
+    }
   }
 
   // playlists is an object with --
   // morningTracks: first sliced half of tracks list from App &
   // eveningTracks: second sliced half of tracks list from App
   //move playlists to be state
-  
+
   render() {
-    const playlists = {
-      morningTracks: this.props.tracks.slice(0, props.tracks.length / 2),
-      eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
-    };
 
     const playlistSwitch = {
       // Track was deleted in Playlist.

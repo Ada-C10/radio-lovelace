ead4bae72a32656a6706393958c0bc6c7e3475d6
Hannah <hannahvanseeters@gmail.com>
2018-12-07 13:26:46 -0800

    added songData to state in App function plus onFavoriteChange to track component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import songData from './data/tracks.json';
 
 songData.forEach((song, i) => {
   song.id = i;
+  song.favorite = false; // favorite prop is now set
 });
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state.songData = songData
+  }
+  
   render() {
     return (
       <div className="App">
@@ -17,7 +24,7 @@ class App extends Component {
           <h1 className="page-header--title">Radio Lovelace</h1>
         </header>
         <main className="main">
-          <RadioSet tracks={songData} />
+          <RadioSet tracks={this.state.songData} />
         </main>
       </div>
     );

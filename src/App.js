952d08439cc5dfd43c67fb5761ba9fbfb3a51f19
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-08 19:17:58 -0800

    added logic to app for doign a markfavorite callback and setting original state

--- src/App.js
+++ src/App.js
@@ -7,17 +7,41 @@ import songData from './data/tracks.json';
 
 songData.forEach((song, i) => {
   song.id = i;
-});
+  song.favorited = false;
+})
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      songData: songData
+    }
+  }
+
+  // createSongData = (songData) => {
+  //   return songData.forEach((song, i) => {
+  //     song.id = i;
+  //     song.favorited = false;
+  //   })
+  // }
+
+  markFavorite = (index) => {
+    const updatedSongData = this.state.songData;
+    updatedSongData[index].favorite = true;
+    this.setState({
+      songData: updatedSongData
+    });
+  }
   render() {
+    console.log("data", songData)
+
     return (
       <div className="App">
         <header>
           <h1 className="page-header--title">Radio Lovelace</h1>
         </header>
         <main className="main">
-          <RadioSet tracks={songData} />
+          <RadioSet tracks={this.state.songData} markFavoritedCallback={this.markFavorite}/>
         </main>
       </div>
     );
